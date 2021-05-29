00fd30e4ec57e5d21aa4969c74c5a0db0bf38fde
xah-t <milaaman@rambler.ru>
2021-05-29 11:37:15 +0300

    add closeEvent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PySide2 import QtWidgets, QtCore
+from PySide2 import QtWidgets, QtCore, QtGui
 import kalku
 
 
@@ -9,6 +9,28 @@ class KalkuWindow(QtWidgets.QMainWindow):
         self.ui = kalku.Ui_MainWindow()
         self.ui.setupUi(self)
         self.mykalku = MyKalku()
+
+    def closeEvent(self, event: QtCore.QEvent.Close):
+        reply = QtWidgets.QMessageBox.question(self, "Выход", "Вы действительно хотите выйти?")
+        print(reply)
+        if reply == QtWidgets.QMessageBox.Yes:
+            event.accept()
+            print('Window closed')
+        elif reply == QtWidgets.QMessageBox.No:
+            event.ignore()
+            print('Window not closed')
+
+    def moveEvent(self, event: QtCore.QEvent.Move) -> None:
+        reply = QtCore.QEvent.Move
+        print("окно движется")
+
+
+
+    def event(self, event: QtCore.QEvent) -> bool:
+        print(event.type())
+        if event.type() == QtCore.QEvent.Type.Close:
+            event.setAccepted(False)
+        return QtWidgets.QMainWindow.event(self, event)
 
 
 class MyKalku(QtCore.QThread):
