3e09888b8e18d0d8612f9f57b46ac1deecbe7c4e
xah-t <milaaman@rambler.ru>
2021-05-31 20:35:09 +0300

    create onPBSaveclicked - not end

--- main.py
+++ main.py
@@ -13,25 +13,39 @@ class KalkuWindow(QtWidgets.QMainWindow):
         #self.ui.lineEdit_8.setHidden(True)
         self.mykalku = MyKalku()
         self.initSqlModel()
+        self.ui.pushButton_2.connect(self.onPBSaveclicked)
 
     def initSqlModel(self):
         self.db = QtSql.QSqlDatabase.addDatabase('QSQLITE')
         self.db.setDatabaseName('fiedlist.db')
         self.model = QtSql.QSqlTableModel()
         self.model.setTable('field')
-        self.model.select() # подключение БД
+
+        self.model.select()  # подключение БД
         # инициализация БД(создание)
         self.model.setHeaderData(0, QtCore.Qt.Horizontal, "ID")
-        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Наименованеие")
-        self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Децимальный №")
-        self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Площадь, мм2")
-        self.model.setHeaderData(4, QtCore.Qt.Horizontal, "Глубина, мм")
-        self.model.setHeaderData(5, QtCore.Qt.Horizontal, "Трудоёмкость н/ч")
-        self.model.setHeaderData(6, QtCore.Qt.Horizontal, "Себестоимость, руб.")
+        self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Name")
+        self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Articul №")
+        self.model.setHeaderData(3, QtCore.Qt.Horizontal, "S, mm2")
+        self.model.setHeaderData(4, QtCore.Qt.Horizontal, "L, mm")
+        self.model.setHeaderData(5, QtCore.Qt.Horizontal, "labour, $/h")
+        self.model.setHeaderData(6, QtCore.Qt.Horizontal, "Cost, rub")
 
         self.ui.tableView.setModel(self.model)
-        self.ui.tableView.setColumnHidden(0, True) # скрытие столбцов
+        self.ui.tableView.setColumnHidden(0, True)  # скрытие столбцов
         self.ui.tableView.horizontalHeader().setSectionResizeMode(QtWidgets.QHeaderView.Stretch)
+
+    def onPBSaveclicked(self):
+        index = self.model.rowCount()
+        self.model.insertRows(index, 1)
+        self.model.setData(self.model.index(index, 1), self.ui.lineEdit_1.text())
+        self.model.setData(self.model.index(index, 2), self.ui.lineEdit_2.text())
+        self.model.setData(self.model.index(index, 3), self.ui.lineEdit_3.text())
+        self.model.setData(self.model.index(index, 4), self.ui.lineEdit_4.text())
+        self.model.setData(self.model.index(index, 5), self.ui.lineEdit_5.text())
+        self.model.setData(self.model.index(index, 6), self.ui.lineEdit_6.text())
+        self.model.submitAll()
+        #print(onPBSaveclicked)
 
     def closeEvent(self, event: QtCore.QEvent.Close):
         reply = QtWidgets.QMessageBox.question(self, "Выход", "Вы действительно хотите выйти?")
