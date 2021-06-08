ddfa6809de36659a56026dd93fc565451ca56910
xah-t <milaaman@rambler.ru>
2021-06-08 20:24:54 +0300

    add core Vremyapartii

--- main.py
+++ main.py
@@ -123,12 +123,13 @@ class MyKalkul(QtCore.QThread):
         print(self.ploshad)
         print(self.glubina)
         skorost = int(self.ploshad) * 1.8 # 1.8 - стандартное время обработки 1мм2
-        skorostsloev = int(self.glubina) * skorost
+        skorostsloev = int(self.glubina) * skorost # кол-во слоев * Nмм2/сек
         resultTrud = skorostsloev / 3600 # трудоемкость = площадь обработки*кол-во секунт / 3600(перевод в н/ч)
         resultSebest = resultTrud * 2350 # в дальнейшем - вписать ссылку на lineEdit с ценой нормочаса
-        vremyapartii = skorostsloev * int(self.kolichestvo)
+        vremyapartii = skorostsloev * int(self.kolichestvo) / 3600
         print(f"Трудоёмкость {resultTrud}")
         print(f"Себестоимость {resultSebest}")
+        print(f"Время обработки партии {vremyapartii} часов")
 
         self.signalTrudoemkost.emit(str(resultTrud))
         self.signalSebestoimost.emit(str(resultSebest))
