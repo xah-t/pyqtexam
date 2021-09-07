3bf85263f42e70d3b1c380e92dc10b978cf135c2
xaht <xah-t>
2021-09-07 14:41:43 +0300

    add table work_cost

--- main.py
+++ main.py
@@ -40,17 +40,22 @@ class KalkuWindow(QtWidgets.QMainWindow):
         self.db.setDatabaseName('C:/python/VKR/pyqtexam/fieldlist_var2.db')  # C:/Users/nva10/Downloads/fieldlist_var2.db
         self.model = QtSql.QSqlTableModel()
         self.model.setTable('fieldlist')  # название таблицы, а не БД
+        self.model1 = QtSql.QSqlTableModel()
+        self.model1.setTable('work_cost')  # название таблицы, а не БД
 
         self.model.select()  # подключение БД
         # инициализация БД(создание)
         self.model.setHeaderData(0, QtCore.Qt.Horizontal, "ID")
         self.model.setHeaderData(1, QtCore.Qt.Horizontal, "Децимальный №")
         self.model.setHeaderData(2, QtCore.Qt.Horizontal, "Наименование")
-        self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Материал")
-        self.model.setHeaderData(4, QtCore.Qt.Horizontal, "Норма расхода материала, кг")
-        self.model.setHeaderData(5, QtCore.Qt.Horizontal, "Площадь поверхности, мм2")
-        self.model.setHeaderData(6, QtCore.Qt.Horizontal, "Глубина обработки, мм")  # заменить на стоимость изделия(сложить трудоемкость и материал)
-        self.model.setHeaderData(7, QtCore.Qt.Horizontal, "Срок изготовления, час")
+        #self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Материал")
+        #self.model.setHeaderData(4, QtCore.Qt.Horizontal, "Норма расхода материала, кг")
+        self.model.setHeaderData(3, QtCore.Qt.Horizontal, "Площадь поверхности, мм2")
+        self.model.setHeaderData(4, QtCore.Qt.Horizontal, "Глубина обработки, мм")  # заменить на стоимость изделия(сложить трудоемкость и материал)
+        self.model.setHeaderData(5, QtCore.Qt.Horizontal, "Трудоемкость, н/ч")
+        self.model.setHeaderData(6, QtCore.Qt.Horizontal, "Себестоимость, руб. с НДС")
+        self.model1.select()
+        self.model1.setHeaderData(4, QtCore.Qt.Horizontal, "Время изготовления партии, дней")
 
         self.ui.tableView.setModel(self.model)
         self.ui.tableView.setColumnHidden(0, True)  # скрытие столбцов
@@ -71,22 +76,20 @@ class KalkuWindow(QtWidgets.QMainWindow):
         self.model.setData(self.model.index(index, 1), self.ui.LEArticul.text())
         self.model.setData(self.model.index(index, 2), self.ui.LENaimenovanie.text())
         self.model.setData(self.model.index(index, 3), self.ui.LEPloshad.text())  # LEMaterial
-        self.model.setData(self.model.index(index, 4), self.ui.LEGlubina.text())  # LEMaterial_rate
+        self.model.setData(self.model.index(index, 4), self.ui.LEGlubina.text())  # LEMaterialrate
         self.model.setData(self.model.index(index, 5), self.ui.LETrudoemkost.text())
         self.model.setData(self.model.index(index, 6), self.ui.LESebestoimost.text())
-        self.model.setData(self.model.index(index, 7), self.ui.LEVremyapartii.text())
+        self.model1.setData(self.model1.index(index, 4), self.ui.LEVremyapartii.text())   # заменить на столбец из таблицы work_cost
         self.model.submitAll()
 
         print('onPBSaveclicked')
 
     def onPBMoveclicked(self):
         if self.ui.tableView.currentIndex().row() > -1:
-
             index_list = []
             for model_index in self.ui.tableView.selectionModel().selectedRows():
                 index = QtCore.QPersistentModelIndex(model_index)
                 index_list.append(index)
-
             for index in index_list:
                  self.model.removeRow(index.row())
         else:
