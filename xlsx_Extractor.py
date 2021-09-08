852e81fde04942c9995fa7a674c1d858a0066bf2
xaht <milaaman@rambler.ru>
2021-09-08 15:08:11 +0300

    add xlsx_Extractor - it's work, but need vremyapartii

--- xlsx_Extractor.py
+++ xlsx_Extractor.py
@@ -1,14 +1,16 @@
 import datetime
 import xlsxwriter
+import sqlite3
 
+con = sqlite3.connect('fieldlist_var2.db')
+expenses = []
+with con:
+    cur = con.cursor()
+    cur.execute("SELECT * FROM fieldlist")  # ORDER BY id DESC LIMIT 3
+    for row in cur:
+        expenses.append(row)
+    #print(expenses)
 
-expenses = (
-    ['rgrty4557', "shina", 23.55, 245.6, 6],
-    ['Gas245', 'opora', 2.54, 300.54, 13],
-    ['Fo345od', "Palka", 1.234, 23535, 28],
-    ['G13ym', "srert", 13, 45.36, 6],
-    ['er45od', "Pachka", 1.2, 25, 2]
-)
 # Create an new Excel file and add a worksheet.
 workbook = xlsxwriter.Workbook('Реестр_' + str(datetime.date.today()) + '.xlsx')
 worksheet = workbook.add_worksheet(str(datetime.date.today()))
@@ -16,36 +18,23 @@ worksheet = workbook.add_worksheet(str(datetime.date.today()))
 # Add a bold format to use to highlight cells.
 bold = workbook.add_format({'bold': True})
 
-# Write some data headers.
-worksheet.write('A1', 'Децимальный №', bold)
-worksheet.write('B1', 'Наименование', bold)
-worksheet.write('C1', 'Трудоемкость, н/ч', bold)
-worksheet.write('D1', 'Себестоимость, руб. с НДС', bold)
-worksheet.write('E1', 'Срок изготовления партии,  дней', bold)
+# Write data headers.
+worksheet.write('A1', '№ п/п', bold)
+worksheet.write('B1', 'Децимальный №', bold)
+worksheet.write('C1', 'Наименование', bold)
+worksheet.write('D1', 'Площадь поверхности, мм2', bold)
+worksheet.write('E1', 'Глубина обработки, мм"', bold)
+worksheet.write('F1', 'Трудоемкость, н/ч', bold)
+worksheet.write('G1', 'Себестоимость, руб. с НДС', bold)
+worksheet.write('H1', 'Срок изготовления партии,  дней', bold)
 
-row = 1
-col = 0
-for articul, naimenovanie, trudoemkost, sebestoimost, vremyapartii in (fieldlist):
-    worksheet.write(row, col, articul)
-    worksheet.write(row, col + 1, naimenovanie)
-    worksheet.write(row, col + 2, trudoemkost)
-    worksheet.write(row, col + 3, sebestoimost)
-    worksheet.write(row, col + 4, vremyapartii)
-    row += 1
-for row in range(0, 5):
-    worksheet.write(row+7, 0, 'Hello')
+# Write some data.
+for num_row, row_data in enumerate(expenses):  # expenses
+    for num_col, col_data in enumerate(row_data):
+        worksheet.write(num_row+1, num_col, col_data)
+
 # Widen the first column to make the text clearer.
 #worksheet.set_column('A:A', 20)
-
-# Write some simple text.
-#worksheet.write('A1', 'Hello', bold)
-
-# Text with formatting.
-#worksheet.write('A2', 'World', bold)
-
-# Write some numbers, with row/column notation.
-#worksheet.write(2, 0, 123)
-#worksheet.write(3, 0, 123.456)
 
 # Insert an image.
 #worksheet.insert_image('B5', 'logo.png')
