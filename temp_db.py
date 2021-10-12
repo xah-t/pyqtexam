af1f6771cd573e3cedff3b5bff9904d8c4aa0ffd
xaht <milaaman@rambler.ru>
2021-10-12 23:57:30 +0300

    temp_db: заполнил таблицу с ценами

--- temp_db.py
+++ temp_db.py
@@ -15,11 +15,9 @@ values_ = []
 path = 'C:/python/VKR/pyqtexam/material_catalog.xlsx'
 wb_obj = openpyxl.load_workbook(path)
 sheet_obj = wb_obj.active
-num_row = 0
 connect_to_db = sqlite3.connect('fieldlist_var2.db')
 cursor_material_cost_ = connect_to_db.cursor()
 for i in range(1, sheet_obj.max_row):
-    #for j in range(1, sheet_obj.max_row):
     cell_obj = sheet_obj.cell(row=i, column=1)
     cell_obj_1 = sheet_obj.cell(row=i, column=2)
     values_.append(cell_obj.value)
@@ -28,17 +26,9 @@ for i in range(1, sheet_obj.max_row):
     sql_ = "insert into material_cost (material_mark, material_cost_rub) values(?, ?)"
     cursor_material_cost_.execute(sql_, values_)
     values_ = []
-    num_row += 1
-connect_to_db.commit()
+#connect_to_db.commit()
 print(values_)
-"""
-connect_to_db = sqlite3.connect('fieldlist_var2.db')
-cursor_material_cost_ = connect_to_db.cursor()
-for i in range(1, num_row):  # len(list(values_))
-    sql_ = "insert into material_cost (material_mark, material_cost_rub) values(?, ?)"
-    #values_ = ["J23", 296]
-    cursor_material_cost_.execute(sql_, values_)  # cursor_material_cost_.execute("insert into material_cost (material_mark, material_cost_rub) values ('Д16', 275), ('Д16т', 295)")
-connect_to_db.commit()"""
+
 cursor_material_cost_.execute("SELECT * FROM material_cost")
 for row in cursor_material_cost_:
     material_cost_.append(row)
