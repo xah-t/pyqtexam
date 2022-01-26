87a90070c88b9e7ab922cb62cad7a61046df2258
xah-t <milaaman@rambler.ru>
2022-01-27 00:29:28 +0300

    Убраны лишние принты и старые вырсии окон. Добавлена документация. Пересобрать БД, удалить лишние ID.

--- temp_db.py
+++ temp_db.py
@@ -1,31 +1,9 @@
-import datetime
-import xlsxwriter
-#from xlrd import open_workbook
-import subprocess
 import sqlite3
-import openpyxl
-
-
-def download_catalog(path):
-
-    """Открываю xlsx файл, записываю в material_cost"""
-    wb_obj = openpyxl.load_workbook(path)
-    sheet_obj = wb_obj.active
-    connect_to_db = sqlite3.connect('fieldlist_var2.db')
-    cursor_connect_to_db = connect_to_db.cursor()
-    for i in range(1, sheet_obj.max_row):
-        cell_obj = sheet_obj.cell(row=i, column=1)
-        cell_obj_1 = sheet_obj.cell(row=i, column=2)
-        values_ = (cell_obj.value, cell_obj_1.value)
-        print(values_)
-        sql_ = "insert into material_cost (material_mark, material_cost_rub) values(?, ?)"
-        cursor_connect_to_db.execute(sql_, values_)
-    # connect_to_db.commit()
 
 
 def update_table_work_cost():
+    """Обновление таблицы "work_cost" в БД в соответствии с таблицей "fieldlist". Сравнение по артикулам."""
 
-    """Открываю БД, выбираю из fieldlist артикулы и подстваляю в work_cost"""
     values_ = []
     connect_to_db = sqlite3.connect('fieldlist_var2.db')
     cursor_connect_to_db = connect_to_db.cursor()
@@ -40,26 +18,8 @@ def update_table_work_cost():
         cursor_connect_to_db.execute(sql_, val)
     # connect_to_db.commit()
 
-def check_synchro_in_db(val):
-    connect_to_db = sqlite3.connect('fieldlist_var2.db')
-    cursor_connect_to_db = connect_to_db.cursor()
-    cur = ''
-    cursor_connect_to_db.execute("SELECT articul from fieldlist where articul = ?", (val,))
-    try:
-        cur = cursor_connect_to_db.fetchone()[0]
-        if val == cur:
-            return True
-    except TypeError:
-        return False
-
-
-
 
 if __name__ == "__main__":
-    #path = 'C:/python/VKR/pyqtexam/material_catalog.xlsx'
-    #download_catalog(path)
-    #update_table_work_cost()
-    #value_ = str("КЕ.356.655")
-    #value2 = str("КЕ.356.6523")
-    #check_synchro_in_db(value_)
+    update_table_work_cost()
+
 
